951c44a6e9a672f99cec2252a83640e8158c4fcd
Can Zhao <zhaocan0928@gmail.com>
2023-04-23 20:42:10 +1000

    move url to recipeDB

--- src/components/RecipeDB/index.js
+++ src/components/RecipeDB/index.js
@@ -1,5 +1,9 @@
 export const RecipeDB_API_KEY = process.env.REACT_APP_RecipeDB_API_KEY;
-export const RecipeDB_API_URL = process.env.REACT_APP_RecipeDB_API_URL;
+export const RecipeDB_API_URL = "tasty.p.rapidapi.com";
+
+// mock test
+// export const RecipeDB_API_URL =
+//   "c426c851-594c-4e6d-b7c2-fa3c01b85cfc.mock.pstmn.io";
 
 export const TopSearchs = ["noodle", "fish", "tomato", "fried rice"];
 export const HistorySearchs = ["bean", "fish", "pork"];
